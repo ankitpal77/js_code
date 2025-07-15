7f36307ccfeada1abe9d8c4f2f3836525830d1a1
Ankit Pal <ankit.pal.cse1@gmail.com>
2025-07-15 19:00:13 +0000

    container config done

---
+++ .devcontainer/01_basics/01_varibles.js
@@ -0,0 +1,11 @@
+const accountId = "29546"
+let accountEmail = "ankit@gamil.com"
+var accountPassword = "1234567"
+accountCity = "Jaunpur"
+
+accountEmail = "ankit@hknp"
+accountPassword ="14552"
+accountCity = "meerut"
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity]);
